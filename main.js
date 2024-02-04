cc154e03965ec80842f7b1cbcad6cea7b2a11185
memonisha <75033521+memonisha@users.noreply.github.com>
2024-02-05 03:02:54 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,38 @@
+
+
+function setup() {
+  canvas = createCanvas(280, 280);
+  canvas.center();
+  background("white");
+  
+
+
+}
+
+function preload() {
+
+
+ 
+}
+
+
+
+function clearCanvas() {
+
+  background("white");
+}
+
+function draw() {
+
+ 
+}
+
+function classifyCanvas() {
+ 
+}
+
+function gotResult(error, results) {
+ 
+}
+
+
